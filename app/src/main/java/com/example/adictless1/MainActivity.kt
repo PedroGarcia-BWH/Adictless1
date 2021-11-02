8baa97fa1b80d76380a857ab7837e5976ea77660
saikora11 <pedro.garciaromera@alum.uca.es>
2021-11-02 17:01:46 +0100

    version con bd

--- app/src/main/java/com/example/adictless1/MainActivity.kt
+++ app/src/main/java/com/example/adictless1/MainActivity.kt
@@ -3,7 +3,14 @@ package com.example.adictless1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.vishnusivadas.advanced_httpurlconnection.PutData
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +18,58 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val registrar=findViewById<Button>(R.id.register)
-        registrar.setOnClickListener {
+        val log_email = findViewById<TextView>(R.id.email)
+        val log_password = findViewById<TextView>(R.id.password)
+        val log_login = findViewById<Button>(R.id.login)
+        val progressBar = findViewById<ProgressBar>(R.id.progress)
+
+        log_login.setOnClickListener(View.OnClickListener {
+            val email: String
+            val password: String
+            email = log_email.getText().toString()
+            password = log_password.getText().toString()
+            if (email != "" && password != "") {
+                progressBar.setVisibility(View.VISIBLE)
+                val handler = Handler(Looper.getMainLooper())
+                handler.post {
+                    val field = arrayOfNulls<String>(2)
+                    field[0] = "email"
+                    field[1] = "password"
+                    //Creating array for data
+                    val data = arrayOfNulls<String>(2)
+                    data[0] = email
+                    data[1] = password
+
+                    val putData =
+                        PutData("http://192.168.1.16/LoginRegister/login.php", "POST", field, data)
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            progressBar.setVisibility(View.GONE)
+                            val result = putData.result
+                            if (result == "Inicio de Sesion Correcto") {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT)
+                                    .show()
+                                val intent = Intent(applicationContext, Login::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Todos los campos son requeridos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
+        val registro =findViewById<Button>(R.id.register)
+        registro.setOnClickListener {
             val intento1 = Intent(this, RegisterActivity1::class.java)
             startActivity(intento1)
         }
--- app/src/main/java/com/example/adictless1/RegisterActivity2.kt
+++ app/src/main/java/com/example/adictless1/RegisterActivity2.kt
@@ -1,11 +1,74 @@
 package com.example.adictless1
 
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.ProgressBar
 import android.os.Bundle
+import android.os.Looper
+import com.vishnusivadas.advanced_httpurlconnection.PutData
+import android.widget.Toast
+import android.content.Intent
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class RegisterActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register2)
+
+        val reg_email = findViewById<TextView>(R.id.emailRegister)
+        val reg_username = findViewById<TextView>(R.id.nickNameRegister)
+        val reg_password = findViewById<TextView>(R.id.passwordRegister)
+        val reg_registro = findViewById<Button>(R.id.confirmRegister)
+        val progressBar = findViewById<ProgressBar>(R.id.reg_progress)
+
+        reg_registro.setOnClickListener(View.OnClickListener {
+            val email: String
+            val username: String
+            val password: String
+            email = reg_email.getText().toString()
+            username = reg_username.getText().toString()
+            password = reg_password.getText().toString()
+            if (email != "" && username != "" && password != "") {
+                progressBar.setVisibility(View.VISIBLE)
+                val handler = Handler(Looper.getMainLooper())
+                handler.post {
+                    val field = arrayOfNulls<String>(3)
+                    field[0] = "email"
+                    field[1] = "username"
+                    field[2] = "password"
+                    //Creating array for data
+                    val data = arrayOfNulls<String>(3)
+                    data[0] = email
+                    data[1] = username
+                    data[2] = password
+                    val putData =
+                        PutData("http://192.168.1.16/LoginRegister/signup.php", "POST", field, data)
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            progressBar.setVisibility(View.GONE)
+                            val result = putData.result
+                            if (result == "Registro Correcto") {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT)
+                                    .show()
+                                val intent = Intent(applicationContext, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(applicationContext, result, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Todos los campos son requeridos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
