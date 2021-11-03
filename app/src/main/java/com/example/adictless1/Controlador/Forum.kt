2b2b92b11bc416f48fdbf42e23e40533abdb806d
Argudo <josexerezano7@gmail.com>
2021-11-03 16:44:49 +0100

    Implementación de pestañas

---
+++ app/src/main/java/com/example/adictless1/Controlador/Forum.kt
@@ -0,0 +1,28 @@
+package com.example.adictless1.Controlador
+
+import android.os.Bundle
+import com.example.adictless1.Controlador.Forum
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.adictless1.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Forum.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Forum : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forum, container, false)
+    }
+}
--- app/src/main/java/com/example/adictless1/Login.kt
+++ app/src/main/java/com/example/adictless1/Login.kt
@@ -2,10 +2,30 @@ package com.example.adictless1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.example.adictless1.Controlador.Forum
+import com.example.adictless1.Controlador.Home
+import com.example.adictless1.Controlador.PageController
+import com.example.adictless1.Controlador.Progress
+import com.google.android.material.tabs.TabLayout
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setUpTabs()
+    }
+
+    private fun setUpTabs(){
+        val adapter = PageController(supportFragmentManager)
+        adapter.addFragment(Home(), "Home")
+        adapter.addFragment(Forum(), "Forum")
+        adapter.addFragment(Progress(), "Progress")
+        val viewpager = findViewById<ViewPager>(R.id.viewpager)
+        val tabs = findViewById<TabLayout>(R.id.tabs)
+        viewpager.adapter = adapter
+        tabs.setupWithViewPager(viewpager)
+
     }
 }
