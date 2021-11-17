034ba686b04332b1a68ee05c55302548368462b4
Argudo <josexerezano7@gmail.com>
2021-11-17 10:11:56 +0100

    Version 0.2.2 - CardView Clickable

--- app/src/main/java/com/example/adictless1/Controlador/Home.kt
+++ app/src/main/java/com/example/adictless1/Controlador/Home.kt
@@ -1,11 +1,20 @@
 package com.example.adictless1.Controlador
 
+import android.content.Intent
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.example.adictless1.NewsActivity
 import com.example.adictless1.R
+import com.example.adictless1.RegisterActivity1
+
 
 /**
  * A simple [Fragment] subclass.
@@ -20,9 +29,21 @@ class Home : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val an1CardView = view?.findViewById<CardView>(R.id.announce1)
+        Log.d("View", an1CardView.toString())
+        an1CardView?.setOnClickListener{
+            Log.d("myTag", "This is my message");
+            val an1Ac = Intent(activity, NewsActivity::class.java)
+            activity?.startActivity(an1Ac)
+        }
     }
 
 
--- app/src/main/java/com/example/adictless1/Login.kt
+++ app/src/main/java/com/example/adictless1/Login.kt
@@ -1,19 +1,20 @@
 package com.example.adictless1
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.autofill.OnClickAction
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.viewpager.widget.ViewPager
-import com.example.adictless1.Controlador.Forum
-import com.example.adictless1.Controlador.Home
-import com.example.adictless1.Controlador.PageController
-import com.example.adictless1.Controlador.Progress
+import com.example.adictless1.Controlador.*
 import com.google.android.material.badge.BadgeDrawable
 import com.google.android.material.tabs.TabLayout
+
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +22,6 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         setUpTabs()
-        /*val anuncio1 = findViewById<CardView>(R.id.announce1)
-        anuncio1.setOnClickListener {
-            val an1Act = Intent(this, RegisterActivity1::class.java)
-            startActivity(an1Act)
-        }*/
 
     }
 
