1b34a2015e9fde2bb9cd7b3a5f85051600e2f6a4
saikora11 <pedro.garciaromera@alum.uca.es>
2021-11-18 15:10:43 +0100

    empezando la encuesta retocada

--- app/src/main/java/com/example/adictless1/SurveyActivity1.kt
+++ app/src/main/java/com/example/adictless1/SurveyActivity1.kt
@@ -1,11 +1,30 @@
 package com.example.adictless1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TextView
 
 class SurveyActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey1)
+
+       /* val checkBoxYes = (CheckBox)findViewById(R.id.p3Yes)
+        if(checkBoxYes.isChecked())
+        {
+
+            val redesSociales =findViewById<TextView>(R.id.bottonSurvey1)
+            redesSociales.setOnClickListener{
+                val intentRedesSociales = Intent(this,SurveyActivity2::class.java)
+                startActivity(intentRedesSociales)
+            }
+        }
+        val survey =findViewById<TextView>(R.id.survey)
+        survey.setOnClickListener {
+            val surveyAct = Intent(this, SurveyActivity1::class.java)
+            startActivity(surveyAct)
+        }*/
     }
 }
---
+++ app/src/main/java/com/example/adictless1/SurveyActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.adictless1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SurveyActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey2)
+    }
+}
