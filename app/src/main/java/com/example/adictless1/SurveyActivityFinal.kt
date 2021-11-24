ec5aa5ae0ec94a76f9284be2355b5f32a075fdbd
Saikora11 <pedro.garciaromera@alum.uca.es>
2021-11-24 21:53:58 +0100

    version 0.6.0 encuesta da resultado, crashea cuando le das a si redes y no intrudices el nRedes, hay que implementar campos obligatorios

--- app/src/main/java/com/example/adictless1/SurveyActivityFinal.kt
+++ app/src/main/java/com/example/adictless1/SurveyActivityFinal.kt
@@ -1,19 +1,18 @@
 package com.example.adictless1
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.RadioButton
-import android.widget.TextView
+import android.widget.*
 
 class SurveyActivityFinal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey_final)
-
+        var RDactivate = false
         val redesLayout = findViewById<LinearLayout>(R.id.RedesLayout)
         redesLayout.visibility = View.GONE
 
@@ -22,9 +21,11 @@ class SurveyActivityFinal : AppCompatActivity() {
 
         redesActivate.setOnClickListener{
             redesLayout.visibility = View.VISIBLE
+            RDactivate = true
         }
         redesDeactivate.setOnClickListener{
             redesLayout.visibility = View.GONE
+            RDactivate = false
         }
 
         val apuestasLayout = findViewById<LinearLayout>(R.id.layoutApuesta)
@@ -53,11 +54,108 @@ class SurveyActivityFinal : AppCompatActivity() {
             juegosLayout.visibility = View.GONE
         }
 
-        val continuar =findViewById<Button>(R.id.bottonSurvey)
-        continuar.setOnClickListener {
-            val continuarAct = Intent(this, RegisterActivity2::class.java)
-            startActivity(continuarAct)
+        var contRedesSociales=0
+
+        var contApuesta=0
+
+        var contGames=0
+
+
+        val p6RS = findViewById<RadioButton>(R.id.p6Yes)
+        p6RS.setOnClickListener{
+            contRedesSociales++
         }
 
+        val p7RS = findViewById<RadioButton>(R.id.p7Yes)
+        p7RS.setOnClickListener{
+            contRedesSociales++
+        }
+
+        val p8RS = findViewById<RadioButton>(R.id.p8Yes)
+        p8RS.setOnClickListener{
+            contRedesSociales++
+        }
+        val p13A = findViewById<RadioButton>(R.id.p13Yes)
+        p13A.setOnClickListener{
+
+            contApuesta++
+        }
+
+        val p12A = findViewById<RadioButton>(R.id.p12Yes)
+        p12A.setOnClickListener{
+            contApuesta++
+        }
+
+        val p11A = findViewById<RadioButton>(R.id.p11Yes)
+        p11A.setOnClickListener{
+            contApuesta++
+        }
+
+        val p14A = findViewById<RadioButton>(R.id.p14Yes)
+        p14A.setOnClickListener{
+            contApuesta++
+        }
+
+        val p16J = findViewById<RadioButton>(R.id.p16Online)
+        p16J.setOnClickListener{
+            contGames++
+        }
+
+        val p18J = findViewById<RadioButton>(R.id.p18Yes)
+        p18J.setOnClickListener{
+            contGames++
+        }
+
+        val p19J = findViewById<RadioButton>(R.id.p19Yes)
+        p19J.setOnClickListener{
+            contGames++
+        }
+
+        val p29J = findViewById<RadioButton>(R.id.p29Yes)
+        p29J.setOnClickListener{
+            contGames++
+        }
+
+        val p21J = findViewById<RadioButton>(R.id.p21Yes)
+        p21J.setOnClickListener{
+            if(contGames!= 0) contGames += 100
+            if(contApuesta != 0) contApuesta += 100
+            if(contRedesSociales != 0) contRedesSociales +=100
+        }
+
+
+        val continuar =findViewById<Button>(R.id.bottonSurvey)
+        continuar.setOnClickListener {
+
+          if (RDactivate == true) {
+              var RD = findViewById<EditText>(R.id.nRedesSociales)
+              var nRD = RD.getText().toString()
+
+              contRedesSociales = Integer.parseInt(nRD)
+          }
+
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("RESULTADO")
+            if (contRedesSociales >=5 && contApuesta >=2 && contGames >=2){
+                builder.setMessage("Puede necesitar ayuda sobre Redes Sociales, Apuestas y Videojuegos")
+            }
+            else if(contRedesSociales >=5 && contApuesta >=2) builder.setMessage("Puede necesitar ayuda sobre Redes Sociales y Apuestas")
+            else if(contApuesta >=2 && contGames >=2) builder.setMessage("Puede necesitar ayuda sobre Apuestas y VideoJuegos")
+            else if(contRedesSociales >=2 && contGames >=2) builder.setMessage("Puede necesitar ayuda sobre Redes Sociales y Videojuegos")
+            else if(contRedesSociales >=5 ) builder.setMessage("Puede necesitar ayuda sobre Redes Sociales")
+            else if(contApuesta >=2) builder.setMessage("Puede necesitar ayuda sobre Apuestas")
+            else if(contGames >=2) builder.setMessage("Puede necesitar ayuda sobre Videojuegos")
+
+            builder.setCancelable(true)
+
+            builder.setPositiveButton("Continuar", DialogInterface.OnClickListener{ dialog, which ->
+                val continuarAct = Intent(this, RegisterActivity2::class.java)
+                startActivity(continuarAct)
+            })
+
+            val alertDialog = builder.create()
+            alertDialog.show();
+        }
     }
 }
