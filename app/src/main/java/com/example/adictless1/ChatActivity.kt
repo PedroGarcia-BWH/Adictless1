4c06363cfb8d23c900782148ab112a246d815cbe
Saikora11 <pedro.garciaromera@alum.uca.es>
2021-12-01 00:10:00 +0100

    version 0.7.7 implementacion fallida firebase/chat

--- app/src/main/java/com/example/adictless1/ChatActivity.kt
+++ app/src/main/java/com/example/adictless1/ChatActivity.kt
@@ -2,10 +2,16 @@ package com.example.adictless1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
     }
+
+
 }
---
+++ app/src/main/java/com/example/adictless1/Mensaje.java
@@ -0,0 +1,65 @@
+package com.example.adictless1;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Mensaje  {
+
+        public Mensaje(String mensaje, String nombre, String fotoPerfil, String type_mensaje) {
+            this.mensaje = mensaje;
+            this.nombre = nombre;
+            this.fotoPerfil = fotoPerfil;
+            this.type_mensaje = type_mensaje;
+        }
+        private String mensaje;
+        private String urlFoto;
+        private String nombre;
+        private String fotoPerfil;
+        private String type_mensaje;
+
+        public Mensaje() {
+        }
+
+
+
+        public String getMensaje() {
+            return mensaje;
+        }
+
+        public void setMensaje(String mensaje) {
+            this.mensaje = mensaje;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getFotoPerfil() {
+            return fotoPerfil;
+        }
+
+        public void setFotoPerfil(String fotoPerfil) {
+            this.fotoPerfil = fotoPerfil;
+        }
+
+        public String getType_mensaje() {
+            return type_mensaje;
+        }
+
+        public void setType_mensaje(String type_mensaje) {
+            this.type_mensaje = type_mensaje;
+        }
+
+        public String getUrlFoto() {
+            return urlFoto;
+        }
+
+        public void setUrlFoto(String urlFoto) {
+            this.urlFoto = urlFoto;
+        }
+    }
+
